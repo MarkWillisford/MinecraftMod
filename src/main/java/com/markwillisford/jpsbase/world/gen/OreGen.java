c9accd9218dd709e565d9b2dbb2a158784053bf1
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-06-12 20:32:19 -0700

    underground generation

--- src/main/java/com/markwillisford/jpsbase/world/gen/OreGen.java
+++ src/main/java/com/markwillisford/jpsbase/world/gen/OreGen.java
@@ -4,6 +4,7 @@ import com.markwillisford.jpsbase.init.BlockInitNew;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.placement.ConfiguredPlacement;
@@ -14,16 +15,48 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class OreGen {
 	public static void generateOre() {
 		for (Biome biome : ForgeRegistries.BIOMES) {
-			ConfiguredPlacement<CountRangeConfig> customConfig = Placement.COUNT_RANGE
-				.configure(new CountRangeConfig(20, 5, 0, 128));	// rarity, lowest y, ?, highest y
+			// Diamond: 8 - 1, 0, 0, 16
+			// Gold: 9 - 2, 0, 0, 32
+			// Iron: 9 - 20, 0, 0, 64
+			// Coal: 17 - 20, 0, 0, 128
+			// Andisite: 33 - 10, 0, 0, 80
+			ConfiguredPlacement<CountRangeConfig> gemConfig = Placement.COUNT_RANGE
+				.configure(new CountRangeConfig(1, 0, 0, 16));	// rarity, lowest y, ?, highest y
+			ConfiguredPlacement<CountRangeConfig> alternateStoneConfig = Placement.COUNT_RANGE
+					.configure(new CountRangeConfig(5, 32, 0, 80));	
+			
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
 				Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-				BlockInitNew.RUBY_ORE.get().getDefaultState(), 10)) // this int is the max vain size
-				.withPlacement(customConfig));
+				BlockInitNew.RUBY_ORE.get().getDefaultState(), 2)) // this int is the max vain size
+				.withPlacement(gemConfig));
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
 					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-					BlockInitNew.SAPPHIRE_ORE.get().getDefaultState(), 10))
-					.withPlacement(customConfig));
+					BlockInitNew.SAPPHIRE_ORE.get().getDefaultState(), 2))
+					.withPlacement(gemConfig));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+					BlockInitNew.MARBLE.get().getDefaultState(), 33))
+					.withPlacement(alternateStoneConfig));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+					BlockInitNew.SLATE.get().getDefaultState(), 33))
+					.withPlacement(alternateStoneConfig));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+					BlockInitNew.SLATE_BLUE.get().getDefaultState(), 33))
+					.withPlacement(alternateStoneConfig));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+					BlockInitNew.SLATE_GREEN.get().getDefaultState(), 33))
+					.withPlacement(alternateStoneConfig));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+					BlockInitNew.SLATE_PURPLE.get().getDefaultState(), 33))
+					.withPlacement(alternateStoneConfig));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+					Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+					BlockInitNew.SLATE_RED.get().getDefaultState(), 33))
+					.withPlacement(alternateStoneConfig));
 			/*if (biome == Biomes.PLAINS) {
 				ConfiguredPlacement<CountRangeConfig> customConfig = Placement.COUNT_RANGE
 						.configure(new CountRangeConfig(20, 5, 0, 128));
